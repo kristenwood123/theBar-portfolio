a9838185bb08f46d1884f6959b481b290693f8f3
Kristen Woodward <kristenwood123@gmail.com>
2020-11-23 18:42:25 -0800

    added arrows to images

--- app.js
+++ app.js
@@ -4,3 +4,61 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+let sliderImages = document.querySelectorAll('.slide')
+arrowLeft = document.getElementById('arrow-left')
+arrowRight = document.getElementById('arrow-right')
+current = 0;
+
+//Clear All images
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = 'none' 
+  }
+}
+
+//Initializes slider
+function startSlideshow() {
+  reset()
+  sliderImages[0].style.display ='block'
+}
+
+//show next
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = "block"
+  current++;
+}
+
+// Show previous
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = 'block'
+  current--;
+}
+
+//Left arrow click
+arrowLeft.addEventListener('click', function(){
+  if(current === 0) {
+    current = sliderImages.length
+  }
+  slideLeft();
+})
+
+//Right arrow click
+arrowRight.addEventListener('click', function(){
+  if(current === sliderImages.length - 1) {
+    current = -1
+  }
+  slideRight();
+})
+
+startSlideshow();
+
+
+
+
+
+
+
+
